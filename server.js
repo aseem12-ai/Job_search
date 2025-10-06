dac0a250fa6ddd10afc61095578f545bf3a54e74
Mohammad Aseem <aseemmohammad73@gmail.com>
2025-10-06 14:36:28 +0530

    third commit

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
-import jsonServer from "json-server";
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
 
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-server.use(middlewares);
-server.use(router); // 👈 removed "/api"
+const app = express();
 
-const PORT = process.env.PORT || 3001;
-server.listen(PORT, () => {
-  console.log(`✅ JSON Server running on port ${PORT}`);
+// --- API route ---
+app.get("/api/companies", (req, res) => {
+  const data = JSON.parse(fs.readFileSync(path.join(__dirname, "db.json")));
+  res.json(data.companies);
 });
+
+// --- Serve React frontend in production ---
+app.use(express.static(path.join(__dirname, "dist")));
+
+// app.get("/*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "dist", "index.html"));
+// });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- src/pages/CompanyDirectory.jsx
+++ src/pages/CompanyDirectory.jsx
@@ -4,7 +4,9 @@ import CompanyTable from '../components/CompanyTable'
 import Pagination from '../components/Pagination'
 
 
-const API ='http://localhost:3001/companies'
+const API = import.meta.env.VITE_API_URL || "/api/companies";
+
+
 
 
 const CompanyDirectory = () => {
@@ -20,29 +22,24 @@ const CompanyDirectory = () => {
   const perPage=9;
 
 
-
-  useEffect(()=>{
-    const fetchCompanies=async()=>{
-      try{
-        setLoading(true)
-        const res=await fetch(API)
-        if(!res.ok) throw new Error("Failed to fetch data")
-          const data=await res.json()
-        setCompanies(data)
-        setFiltered(data)
-
-      }
-      catch(err){
-        setError(err.message)
-
-
-      }
-      finally{
-        setLoading(false)
+  useEffect(() => {
+    const fetchCompanies = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(API); // will hit /api/companies
+        if (!res.ok) throw new Error("Failed to fetch data");
+        const data = await res.json();
+        setCompanies(data);
+        setFiltered(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
       }
     };
     fetchCompanies();
-  },[])
+  }, []);
+
 
 
 
@@ -72,7 +69,7 @@ const CompanyDirectory = () => {
 
   const totalPages=Math.ceil(filtered.length / perPage)
   const paginated=filtered.slice((page-1)*perPage,page*perPage);
-  console.log(paginated)
+  
 
 
   if(loading) return <p className='text-center'>Loading Companies...</p>
