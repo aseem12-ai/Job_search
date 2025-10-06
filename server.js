3b672ed209e316d95a4fc17be62d2765229271cf
Mohammad Aseem <aseemmohammad73@gmail.com>
2025-10-06 14:56:52 +0530

    fourth commit

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ app.get("/api/companies", (req, res) => {
 // --- Serve React frontend in production ---
 app.use(express.static(path.join(__dirname, "dist")));
 
-// app.get("/*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "dist", "index.html"));
-// });
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
