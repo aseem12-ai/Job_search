ec7ce965549f66cfc79213a9829b084efab79f51
Mohammad Aseem <aseemmohammad73@gmail.com>
2025-10-06 15:02:59 +0530

    fifth commit

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get("/api/companies", (req, res) => {
 // --- Serve React frontend in production ---
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
